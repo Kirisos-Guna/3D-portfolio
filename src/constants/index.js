70dcb96c0d5d2a79c11b3ebff3378dcc70a0d969
unknown <m.gunamurugan2004@gmail.com>
2023-12-13 18:54:32 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,57 @@
+import {
+    contact,
+    css,
+    github,
+    html,
+    javascript,
+    linkedin
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+   
+   
+];
+
+
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/guna232',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/kirisos-guna/',
+    }
+];
+
+
+
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,24 @@
+
+
+import { CTA } from "../components";
+
+
+const Projects = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        My{" "}
+        <span className='blue-gradient_text drop-shadow font-semibold'>
+          Projects
+        </span>
+      </h1>
+
+      
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default Projects;
